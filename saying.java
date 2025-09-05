4a21622482bded4e95cedeaf35c7a1feec4f21de
clairfranc <cfrancis@students.unwsp.edu>
2025-09-04 22:18:27 -0500

    Add files via upload
    Program1

---
+++ saying.java
@@ -0,0 +1,10 @@
+package FavoriteSaying;
+
+public class saying {
+
+	public static void main(String[] args) {
+		System.out.format("%-15s%-15s", "Winners never quit", "\tand quitters never win");
+		
+	}
+
+}
